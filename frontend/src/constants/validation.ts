a6837d380f86312c019be6da2d7109d778475b76
littlemight <mfang555@gmail.com>
2025-04-25 12:59:09 +0800

    refactor(i18n): collapse single use const to strings for easier extraction

--- frontend/src/constants/validation.ts
+++ frontend/src/constants/validation.ts
@@ -2,16 +2,10 @@ export const REQUIRED_ERROR = 'This field is required'
 
 export const INVALID_EMAIL_ERROR = 'Please enter a valid email'
 
-export const INVALID_DROPDOWN_OPTION_ERROR =
-  'Entered value is not a valid dropdown option'
-
 export const CANNOT_TRANSFER_OWNERSHIP_TO_SELF =
   'You cannot transfer ownership to yourself'
-export const INVALID_COUNTRY_REGION_OPTION_ERROR =
-  'Please select a valid country/region from the dropdown list'
 
 // local address field errors
-export const INVALID_POSTAL_CODE_ERROR = 'Please enter a valid postal code'
 export const VALID_POSTAL_CODE_NO_ADDRESS_ERROR =
   'Address cannot be found. Please fill in details manually'
 export const INVALID_BLOCK_UNIT_ERROR = 'Please use numbers and letters only'
