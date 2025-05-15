213c81a03234245f8ad2ad2f4a60e318d9982d11
littlemight <mfang555@gmail.com>
2025-05-15 12:35:53 +0800

    feat(i18n): adress field validation hook

--- frontend/src/constants/validation.ts
+++ frontend/src/constants/validation.ts
@@ -4,11 +4,3 @@ export const INVALID_EMAIL_ERROR = 'Please enter a valid email'
 
 export const CANNOT_TRANSFER_OWNERSHIP_TO_SELF =
   'You cannot transfer ownership to yourself'
-
-// local address field errors
-export const VALID_POSTAL_CODE_NO_ADDRESS_ERROR =
-  'Address cannot be found. Please fill in details manually'
-export const INVALID_BLOCK_UNIT_ERROR = 'Please use numbers and letters only'
-export const INVALID_NON_NUMERICAL_ERROR = 'Please use numbers only'
-export const INVALID_LEVEL_UNIT_ERROR =
-  'Please include both level and unit number'
